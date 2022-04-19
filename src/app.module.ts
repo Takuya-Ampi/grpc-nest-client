e0372bbfecd2571c7ff9c62a710c06367cdb6aea
Takuya-Ampi <ap.takuya@gmail.com>
2022-04-19 10:17:52 +0900

    customers.protoを読み込み

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Transport, ClientsModule } from '@nestjs/microservices';
+import { join } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'CUSTOMERS_PACKAGE',
+        transport: Transport.GRPC,
+        options: {
+          url: '127.0.0.1:30043',
+          package: 'customers',
+          // protoPath: join(__dirname, './proto/customers.proto'),
+          protoPath: join(process.cwd(), './proto/customers.proto'),
+        },
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
